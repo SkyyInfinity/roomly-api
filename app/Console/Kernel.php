f237e6ddae6b372248696ff3e87700ae929beef5
SkyyInfinity <dylan.hautecoeur@gmail.com>
2023-07-04 10:18:59 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -14,17 +15,24 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->call(function () {
+            Log::info('Running cron job');
+            $now = strtotime(now('Europe/Paris'));
             // find all reservations
             $reservations = DB::table('reservations')->get();
             // loop through all reservations
-            foreach ($reservations as $reservation) {
-                $now = strtotime(now('Europe/Paris'));
-                // if the reservation is not confirmed and the reservation time is less than 30 minutes away
-                if ($reservation->end_at < $now) {
-                    // unreserve the room
-                    return DB::table('rooms')->where('id', $reservation->room)->update(['is_reserved' => false]);
+            if($reservations->count() > 0) {
+                foreach ($reservations as $reservation) {
+                    // if the reservation is not confirmed and the reservation time is less than 30 minutes away
+                    if ($reservation->end_at < $now) {
+                        // unreserve the room
+                        Log::info('Unreserving room '.$reservation->room);
+                        return DB::table('rooms')->where('id', $reservation->room)->update(['is_reserved' => false]);
+                    }
+                    Log::info('No reservations to unreserve');
+                    return;
                 }
-                return;
+            } else {
+                Log::info('No reservations found');
             }
         })->everyMinute()->sendOutputTo(storage_path('logs/cron.log'));
     }
