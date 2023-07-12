357a89028f9a653fe9cec1f37b6b0c7a34848624
SkyyInfinity <dylan.hautecoeur@gmail.com>
2023-07-12 12:08:54 +0200

    Update routes.php

---
+++ app/Admin/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Admin\Controllers\HomeController;
+use App\Admin\Controllers\ReservationController;
+use App\Admin\Controllers\RoomController;
+use Illuminate\Routing\Router;
+use Encore\Admin\Facades\Admin;
+use Illuminate\Support\Facades\Route;
+
+Admin::routes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+    'as'            => config('admin.route.prefix') . '.',
+], function (Router $router) {
+
+    $router->get('/', [HomeController::class, 'index'])->name('home');
+    $router->get('/rooms', [RoomController::class, 'gridRoom'])->name('rooms.list');
+    $router->get('/rooms/create', [RoomController::class, 'addRoom'])->name('rooms.create');
+    $router->get('/rooms/{id}', [RoomController::class, 'detailRoom'])->name('rooms.details');
+    $router->get('/rooms/{id}/edit', [RoomController::class, 'editRoom'])->name('rooms.details');
+    $router->get('/reservations', [ReservationController::class, 'gridReservation'])->name('reservations.list');
+});
